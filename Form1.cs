17eacd61197192f88c81a7b5e1a3d8320a1a90de
Mark Cockerill <114414487+MarkCockerill12@users.noreply.github.com>
2024-03-20 11:53:58 +0000

    update

--- Form1.cs
+++ Form1.cs
@@ -1,27 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.Expando;
 using System.Windows.Forms;
 
 namespace Route_Finder
 {
+
     public partial class Form1 : Form
     {
         private Panel listPnl;
         private Panel infoPnl;
+        private Panel shopPnl;
         private Label mapText;
         private Label listText;
         private Label infoText;
+        private TextBox searchShop;
         private System.Windows.Forms.Button expandBtn;
+        private Timer timer;
+        private bool isExpanding = false;
+        private int expandStep = 10;
+        private PointsCreation p = new PointsCreation();
 
         public Form1()
         {
             InitializeComponent();
             setupDisplay();
+            initialiseShop();
 
             this.BackColor = Color.DarkGray;
             this.Size = new Size(900, 600);
+            timer = new Timer();
+            timer.Interval = 10;
+            timer.Tick += Timer_Tick;
+
+            drawMap();
         }
 
         private void setupDisplay()
@@ -56,33 +70,113 @@ namespace Route_Finder
             expandBtn.Click += new EventHandler(this.expandBtn_Click);
             Controls.Add(this.expandBtn);
             expandBtn.BringToFront();
+
+            //pop up panel shopping
+            shopPnl = new Panel { BackColor = Color.Red, Location = new Point(40, 40), Height = (this.Height - (this.Height / 20)), Width = (this.Width - (this.Width / 20)), Visible = false, };
+            Controls.Add(shopPnl);
+            shopPnl.BringToFront();
+
         }
 
-        private void expandBtn_Click(object sender, EventArgs e) // expand the panel, THIS IS NOT FINISHED, IT SHOULD BE A POP UP FOR SHOPPING
+        private void initialiseShop()
         {
-            // expand the panel
-            if (listPnl.Width == this.Width / 3)
+            //create search bar for shopping
+            searchShop = new TextBox { Location = new Point(this.Width / 4, 60), Size = new Size(350, 40), Font = new Font("Arial", 26, FontStyle.Regular), BorderStyle = BorderStyle.None, BackColor = Color.Silver, ForeColor = Color.Black, Visible = false, };
+            Controls.Add(searchShop);
+
+        }
+
+        private void expandBtn_Click(object sender, EventArgs e)
+        {
+            isExpanding = !isExpanding; // Toggle the state
+            if (isExpanding)
             {
-                listPnl.Width = this.Width;
-                listText.Width = 900;
                 expandBtn.Text = "-";
+                shopPnl.Visible = true;
+                searchShop.Visible = true;
+                searchShop.BringToFront();
+                shopPnl.Height = 0; // Set initial height to 0 before expanding
+                shopPnl.Location = new Point((this.Width - shopPnl.Width) / 2, (this.Height - shopPnl.Height) / 2); // Set initial position below the form
+                timer.Start();
             }
             else
             {
-                listPnl.Width = this.Width / 3;
-                listText.Width = 300;
                 expandBtn.Text = "+";
+                timer.Start();
             }
         }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            int expandIncrement = expandStep;
+            int collapseIncrement = expandStep;
+
+            if (isExpanding)
+            {
+                if (shopPnl.Height < this.Height - (this.Height / 20))
+                {
+                    if (shopPnl.Top > (this.Height - shopPnl.Height) / 2)
+                    {
+                        shopPnl.Top -= expandIncrement / 2; // Move the panel up to maintain the middle position
+
+                        shopPnl.Height += expandIncrement; // Increase the height gradually
+                    }
+                    else
+                    {
+                        shopPnl.Height += expandIncrement; // Increase the height gradually
+                    }
+                }
+                else
+                {
+                    timer.Stop();
+                }
+            }
+            else
+            {
+                if (shopPnl.Height > 0)
+                {
+                    if (shopPnl.Top < (this.Height - shopPnl.Height) / 2)
+                    {
+                        shopPnl.Top += collapseIncrement / 2; // Move the panel down to maintain the middle position
+                        shopPnl.Height -= collapseIncrement; // Decrease the height gradually
+                    }
+                    else
+                    {
+                        shopPnl.Height -= collapseIncrement; // Decrease the height gradually
+                    }
+
+                    if (shopPnl.Height <= 0)
+                    {
+                        shopPnl.Visible = false;
+                        searchShop.Visible = false;
+                        timer.Stop();
+                    }
+                }
+            }
+        }
+        private void drawMap()
+        {
+            //loop through all nodes. Placing on a size*size grid. Draw lines to their connections.
+            int size = this.p.getNumberOfNodes();
+            for (int i = 0; i < size; i++)
+            {
+                for (int ii = 0; ii < size; ii++)
+                {
+
+                }
+            }
+
+        }
+
     }
 
-    /*public class Point
+    public class Node
     {
         private int height = 1;
-        private Dictionary<Point, int> connections = new Dictionary<Point, int>();
+        private Dictionary<Node, int> connections = new Dictionary<Node, int>();
         private int x, y;
 
-        public Point(int x, int y, int height)
+        public Node(int x, int y, int height)
         {
             this.x = x; this.y = y; this.height = height;
         }
@@ -97,25 +191,25 @@ namespace Route_Finder
             this.height = height;
         }
 
-        public Dictionary<Point, int> getConnections()
+        public Dictionary<Node, int> getConnections()
         {
             return connections;
         }
 
-        public void setAllConnections(Point p, int dist)
+        public void setAllConnections(Node p, int dist)
         {
             this.connections.Add(p, dist);
         }
 
-        public int getSpecificPoint(Point p)
+        public int getSpecificPoint(Node p)
         {
             return (int)connections[p];
         }
 
-        public List<Point> getConnection()
+        public List<Node> getConnection()
         {
-            List<Point> pointConnections = new List<Point>();
-            foreach (Point p in connections.Keys) { pointConnections.Add(p); }
+            List<Node> pointConnections = new List<Node>();
+            foreach (Node p in connections.Keys) { pointConnections.Add(p); }
             return pointConnections;
         }
 
@@ -127,7 +221,7 @@ namespace Route_Finder
         public string getStringConnectinos()
         {
             String s = "";
-            foreach (Point p in getConnection())
+            foreach (Node p in getConnection())
             {
                 s += p.getStringCoOrd() + ", ";
             }
@@ -137,40 +231,50 @@ namespace Route_Finder
 
     public class PointsCreation
     {
-        public static void Main(string[] args)
+        private const int size = 10;
+        private List<Node> points = new List<Node>();
+
+
+        public PointsCreation()
         {
-            const int size = 10;
-            var points = new List<Point>();
 
             for (int i = 0; i < size; i++)
             {
                 for (int ii = 0; ii < size; ii++)
                 {
-                    points.Add(new Point(i, ii, 1));
+                    points.Add(new Node(i, ii, 1));
                 }
             }
 
             var rnd = new Random();
             int rnd1;
 
-            foreach (Point p in points)
+            foreach (Node p in points)
             {
                 try
                 {
                     for (int i = 0; i < size; i++)
                     {
                         rnd1 = rnd.Next(size * size);
-                        p.setAllConnections(points[rnd1], 5);
+                        p.setAllConnections(points[rnd1], 5); //All connections are directed, ie one way. It will be a pain to make it omnidirectional. 
                     }
                 }
-                catch (Exception e)
+                catch (Exception e) //For when connection has already been made.
                 {
-                    // Handle exception
+                    //Console.WriteLine(e);
                 }
                 Console.WriteLine(p.getStringCoOrd());
                 Console.WriteLine(p.getStringConnectinos());
                 Console.WriteLine("");
             }
         }
-    }*/
+
+        public Node getNode(int index)
+        {
+            return this.points[index];
+        }
+
+        public List<Node> getAllNodes() { return this.points; }
+        public int getNumberOfNodes() { return this.points.Count; }
+    }
 }
