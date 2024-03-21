199e89d07acf8b50c5e0e826b7fd86ce4ac2bd40
Mark Cockerill <114414487+MarkCockerill12@users.noreply.github.com>
2024-03-21 23:38:35 +0000

    update

--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,13 @@ using System.Drawing;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.Expando;
 using System.Windows.Forms;
+using System.IO;
+using System.Linq;
+
+//sources
+//read from file- https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-read-a-text-file-in-C-Sharp/#:~:text=The%20File%20class%20provides%20two,text%20file%20into%20a%20string.
+//search through file https://stackoverflow.com/questions/30078432/read-a-text-file-and-search-for-string-in-memory-efficient-way-and-abort-when-f
+
 
 namespace Route_Finder
 {
@@ -17,11 +24,13 @@ namespace Route_Finder
         private Label listText;
         private Label infoText;
         private TextBox searchShop;
-        private System.Windows.Forms.Button expandBtn;
+        private Button expandBtn;
         private Timer timer;
         private bool isExpanding = false;
         private int expandStep = 10;
         private PointsCreation p = new PointsCreation();
+        private Button searchBtn;
+        private Label shopText;
 
         public Form1()
         {
@@ -32,7 +41,7 @@ namespace Route_Finder
             this.BackColor = Color.DarkGray;
             this.Size = new Size(900, 600);
             timer = new Timer();
-            timer.Interval = 10;
+            timer.Interval = 1;
             timer.Tick += Timer_Tick;
 
             drawMap();
@@ -51,7 +60,9 @@ namespace Route_Finder
             listPnl.SendToBack();
 
             //add text
-            mapText = new Label { Text = "map goes here", ForeColor = Color.White, BackColor = Color.DodgerBlue, Font = new Font("Arial", 20, FontStyle.Regular), BorderStyle = BorderStyle.None, AutoSize = true, AutoEllipsis = false, MaximumSize = new Size(400, 900), Location = new Point(10, 10) };
+            mapText = new Label { Text = "", ForeColor = Color.White, BackColor = Color.DodgerBlue, Font = new Font("Arial", 20, FontStyle.Regular), BorderStyle = BorderStyle.None, AutoSize = false };
+            mapText.Size = new Size(this.Width - listPnl.Width, this.Height - infoPnl.Height); // Set size to cover the available area
+            mapText.Location = new Point((this.Width - mapText.Width) / 2, (this.Height - mapText.Height) / 2); // Center the map panel
             Controls.Add(mapText);
             mapText.BringToFront();
 
@@ -66,7 +77,7 @@ namespace Route_Finder
             infoText.BringToFront();
 
             // button 
-            expandBtn = new System.Windows.Forms.Button { Text = "+", ForeColor = Color.Black, FlatStyle = FlatStyle.Popup, Font = new Font("Arial", 15, FontStyle.Regular), Location = new Point(840, 10), Size = new Size(25, 25), BackColor = Color.Red };
+            expandBtn = new Button { Text = "+", ForeColor = Color.Black, FlatStyle = FlatStyle.Popup, Font = new Font("Arial", 15, FontStyle.Regular), Location = new Point(840, 10), Size = new Size(25, 25), BackColor = Color.Red };
             expandBtn.Click += new EventHandler(this.expandBtn_Click);
             Controls.Add(this.expandBtn);
             expandBtn.BringToFront();
@@ -81,9 +92,18 @@ namespace Route_Finder
         private void initialiseShop()
         {
             //create search bar for shopping
-            searchShop = new TextBox { Location = new Point(this.Width / 4, 60), Size = new Size(350, 40), Font = new Font("Arial", 26, FontStyle.Regular), BorderStyle = BorderStyle.None, BackColor = Color.Silver, ForeColor = Color.Black, Visible = false, };
+            searchShop = new TextBox {Text= "", Location = new Point(this.Width / 4, 60), Size = new Size(350, 40), Font = new Font("Arial", 26, FontStyle.Regular), BorderStyle = BorderStyle.None, BackColor = Color.Silver, ForeColor = Color.Black, Visible = false, };
             Controls.Add(searchShop);
 
+            // seearch button 
+            searchBtn = new Button { Text = "🔎", ForeColor = Color.Black, FlatStyle = FlatStyle.Popup, Font = new Font("Arial", 15, FontStyle.Regular), Location = new Point(((this.Width / 4)+ 350), 60), Size = new Size(40, 40), BackColor = Color.White, Visible = false };
+            searchBtn.Click += new EventHandler(this.searchBtn_Click);
+            Controls.Add(this.searchBtn);
+           
+
+            //add text
+            shopText = new Label { Text = "Shop results", ForeColor = Color.White, BackColor = Color.DodgerBlue, Font = new Font("Arial", 20, FontStyle.Regular), BorderStyle = BorderStyle.None, AutoSize = true, AutoEllipsis = false, MaximumSize = new Size(400, 900), Location = new Point(this.Width / 4, 110), Visible = false };
+            Controls.Add(shopText);
         }
 
         private void expandBtn_Click(object sender, EventArgs e)
@@ -92,12 +112,10 @@ namespace Route_Finder
             if (isExpanding)
             {
                 expandBtn.Text = "-";
-                shopPnl.Visible = true;
-                searchShop.Visible = true;
-                searchShop.BringToFront();
                 shopPnl.Height = 0; // Set initial height to 0 before expanding
                 shopPnl.Location = new Point((this.Width - shopPnl.Width) / 2, (this.Height - shopPnl.Height) / 2); // Set initial position below the form
                 timer.Start();
+                shopPnl.Visible = true;
             }
             else
             {
@@ -105,6 +123,55 @@ namespace Route_Finder
                 timer.Start();
             }
         }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(searchShop.Text);
+
+            string textFile = @"C:\Temp\Data\Authors.txt";
+
+            shopText.Text = searchShop.Text;
+
+            //search for shop
+            Console.WriteLine("searching for shop");
+            if (File.Exists(textFile))
+            {
+                // Read a text file line by line.
+                string[] lines = File.ReadAllLines(textFile);
+                foreach (string line in lines)
+                    Console.WriteLine(line);
+            }
+
+            //COULD ALSO DO 
+            //FileContainsString(textFile, searchShop.Text, true);
+        }
+
+       /* public static bool FileContainsString(string path, string str, bool caseSensitive = true)
+        {
+            if (String.IsNullOrEmpty(str))
+                return false;
+
+            using (var stream = new StreamReader(path))
+                while (!stream.EndOfStream)
+                {
+                    bool stringFound = true;
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        char strChar = caseSensitive ? str[i] : Char.ToUpperInvariant(str[i]);
+                        char fileChar = caseSensitive ? (char)stream.Read() : Char.ToUpperInvariant((char)stream.Read());
+                        if (strChar != fileChar)
+                        {
+                            stringFound = false;
+                            break; // break for-loop, start again with first character at next position
+                        }
+                    }
+                    if (stringFound)
+                        return true;
+                }
+            return false;
+        }*/
+
+
 
         private void Timer_Tick(object sender, EventArgs e)
         {
@@ -129,12 +196,24 @@ namespace Route_Finder
                 else
                 {
                     timer.Stop();
+                    
+                    searchShop.Visible = true;
+                    searchBtn.Visible = true;
+                    shopText.Visible = true;
+                    searchShop.BringToFront();
+                    searchBtn.BringToFront();
+                    shopText.BringToFront();
                 }
             }
             else
             {
                 if (shopPnl.Height > 0)
                 {
+                    
+                    searchShop.Visible = false;
+                    searchBtn.Visible = false;
+                    shopText.Visible = false;
+
                     if (shopPnl.Top < (this.Height - shopPnl.Height) / 2)
                     {
                         shopPnl.Top += collapseIncrement / 2; // Move the panel down to maintain the middle position
@@ -148,7 +227,6 @@ namespace Route_Finder
                     if (shopPnl.Height <= 0)
                     {
                         shopPnl.Visible = false;
-                        searchShop.Visible = false;
                         timer.Stop();
                     }
                 }
@@ -156,17 +234,33 @@ namespace Route_Finder
         }
         private void drawMap()
         {
-            //loop through all nodes. Placing on a size*size grid. Draw lines to their connections.
-            int size = this.p.getNumberOfNodes();
-            for (int i = 0; i < size; i++)
-            {
-                for (int ii = 0; ii < size; ii++)
-                {
+            // Clear the existing drawing on the map panel
+            mapText.Refresh();
 
-                }
+            // Get the Graphics object to draw on the map panel
+            Graphics g = mapText.CreateGraphics();
+
+            // Calculate the cell size based on the number of nodes and the size of the map panel
+            int cellSize = Math.Min(mapText.Width / p.getNumberOfNodes(), mapText.Height / p.getNumberOfNodes());
+
+            // Loop through all nodes and plot them on the map
+            foreach (Node node in p.getAllNodes())
+            {
+                int x = (node.X * cellSize) + cellSize / 2; // Calculate the x-coordinate with an offset to center the nodes
+                int y = (node.Y * cellSize) + cellSize / 2; // Calculate the y-coordinate with an offset to center the nodes
+
+                // Draw a rectangle at the calculated position to represent the node
+                g.FillRectangle(Brushes.Black, x, y, cellSize, cellSize);
             }
 
+            // Dispose of the Graphics object to free resources
+            g.Dispose();
         }
+
+
+
+
+
 
     }
 
@@ -174,11 +268,14 @@ namespace Route_Finder
     {
         private int height = 1;
         private Dictionary<Node, int> connections = new Dictionary<Node, int>();
-        private int x, y;
+        public int X { get; private set; } // X coordinate property
+        public int Y { get; private set; } // Y coordinate property
 
         public Node(int x, int y, int height)
         {
-            this.x = x; this.y = y; this.height = height;
+            this.X = x;
+            this.Y = y;
+            this.height = height;
         }
 
         public int getHeight()
@@ -215,7 +312,7 @@ namespace Route_Finder
 
         public string getStringCoOrd()
         {
-            return x + ":" + y;
+            return X + ":" + Y;
         }
 
         public string getStringConnectinos()
@@ -261,7 +358,7 @@ namespace Route_Finder
                 }
                 catch (Exception e) //For when connection has already been made.
                 {
-                    //Console.WriteLine(e);
+                    Console.WriteLine(e);
                 }
                 Console.WriteLine(p.getStringCoOrd());
                 Console.WriteLine(p.getStringConnectinos());
