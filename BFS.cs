90c1c1a4ed42c642b6b673cb4410398c71ba1547
FosEd <Fosterej@btinternet.com>
2024-03-25 14:19:16 +0000

    aaa

--- BFS.cs
+++ BFS.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,22 +11,31 @@ namespace Route_Finder
     internal class BFS
     {
         private Queue<Node> queue = new Queue<Node> ();
+        private Node target = null;
+        private int distance;
 
-
-
-        public BFS(Node root)
+        public BFS(Node root, Node target)
         {
+            distance = 0;
+            this.target = target;
             queue.Enqueue(root);
             search();
         }
 
-        public void searchNode()
+        private Node searchNode()
         {
+
+
             while (queue.Count > 0)
             {
                 Node node = queue.Dequeue();
                 foreach (Node n in node.getConnection())
                 {
+                    if (n == target)
+                    {
+                        return n;
+                    }
+
                     if (!n.isExplored())
                     {
                         queue.Enqueue(n);
@@ -34,6 +44,8 @@ namespace Route_Finder
                     }       
                 }
             }
+
+            return null;
         }
 
         public void search()
